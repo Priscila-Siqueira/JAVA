16ef4fd8199f2c9a69b89bdda82e9efe87f45deb
ADS 2024.1N <36129382024.1N@senacrjedu.local>
2024-10-03 21:21:37 -0300

    aula- 3/10

--- Java-Priscila/orientacaoaobjetos1/src/br/senac/rj/banco/modelo/Conta.java
+++ Java-Priscila/orientacaoaobjetos1/src/br/senac/rj/banco/modelo/Conta.java
@@ -3,19 +3,40 @@ package br.senac.rj.banco.modelo;
 public class Conta {
 
 
-	public int agencia;
-	public int numero;
-	public double saldo;
+	private int agencia;
+	private int numero;
+	private double saldo;
 	
+	
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
 	public void deposita(double valor) {
-		saldo = saldo + valor;
+		this.saldo = this.saldo + valor;
 	}
 	
 	public boolean saca (double valor) {
 		double novoSaldo = saldo - valor;
 		if (novoSaldo <0)
 			return false;
-		saldo = novoSaldo;
+		this.saldo = novoSaldo;
 		return true;
 	}
 	
