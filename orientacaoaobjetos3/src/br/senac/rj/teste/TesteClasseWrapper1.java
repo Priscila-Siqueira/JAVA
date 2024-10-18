0971682a125536473a638ee714ce55d59ed62e30
ADS 2024.1N <36129382024.1N@senacrjedu.local>
2024-10-17 21:25:40 -0300

    aula17/10

---
+++ orientacaoaobjetos3/src/br/senac/rj/teste/TesteClasseWrapper1.java
@@ -0,0 +1,27 @@
+package br.senac.rj.teste;
+
+public class TesteClasseWrapper1 {
+
+	public static void main(String[] args) {
+		//Unboxing
+		boolean a = Boolean.parseBoolean("true");
+		byte b = Byte.parseByte("2");
+		short c = Short.parseShort("10");
+		int d = Integer.parseInt("20");
+		long e = Long.parseLong("30");
+		float f = Float.parseFloat("40.5");
+		double g = Double.parseDouble("50.6");
+		// Autoboxing
+		Integer h = 800;
+		//Exibição de valores
+		System.out.println("a=" + a);
+		System.out.println("b=" +b);
+		System.out.println("c=" + c);
+		System.out.println("d=" + d);
+		System.out.println("e=" + e);
+		System.out.println("f=" + f);
+		System.out.println("=" + g);
+		System.out.println("largura de h ="+h.toString().length());
+	}
+
+}
--- projetos/orientacaoaobjetos2/src/br/senac/rj/empresa/teste/TesteFuncionario1.java
+++ projetos/orientacaoaobjetos2/src/br/senac/rj/empresa/teste/TesteFuncionario1.java
@@ -1,19 +1,82 @@
 package br.senac.rj.empresa.teste;
 
 import br.senac.rj.empresa.modelo.Funcionario;
+import java.util.Scanner;
 
 public class TesteFuncionario1 {
 
-	public static void main(String[] args) {
-		Funcionario func1 = new Funcionario();
-		func1.registro = 1;
-		func1.nome = "Zezinho";
-		func1.cargo = 1;
-		func1.salario = 10000;
-		
-		System.out.println("Salário = " + func1.salario);
-		
-		func1.aplicarBonificacao();
-		System.out.println("O novo salário de " + func1.nome + " é " + func1.salario);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Funcionario func1 = new Funcionario();
+
+        // Solicitar e ler o registro do funcionário
+        System.out.print("Digite o registro do funcionário: ");
+        func1.registro = Integer.parseInt(sc.nextLine());
+
+        // Solicitar e ler o nome do funcionário
+        System.out.print("Digite o nome do funcionário: ");
+        func1.nome = sc.nextLine();
+
+        // Solicitar e ler o cargo do funcionário
+        System.out.print("Digite o cargo do funcionário (número): ");
+        func1.cargo = Integer.parseInt(sc.nextLine());
+
+        // Solicitar e ler o salário do funcionário
+        System.out.print("Digite o salário do funcionário: ");
+        func1.salario = Double.parseDouble(sc.nextLine());
+
+        // Imprimir o salário antes da bonificação
+        System.out.println("Salário = " + func1.salario);
+
+        // Aplicar a bonificação
+        func1.aplicarBonificacao();
+
+        // Imprimir o novo salário
+        System.out.println("O novo salário de " + func1.nome + " é " + func1.salario);
+        
+        System.out.println("Deseja continuar? ");
+		String Pergunta  = sc.nextLine();
+
+        // Fechar o scanner
+        sc.close();
+        
+        
+    }
 }
+
+
+
+/*package br.senac.rj.empresa.modelo;
+
+public class Funcionario {
+public int registro;
+public String nome;
+public int cargo;
+public double salario;
+
+public void aplicarBonificacao() {
+double bonificacao;
+switch (this.cargo) {
+case 1:
+bonificacao = this.salario * 0.2;
+break;
+case 2:
+bonificacao = this.salario * 0.4;
+break;
+case 3:
+bonificacao = this.salario * 0.6;
+break;
+default:
+bonificacao = 0;
+break;
+}
+System.out.println("Bonificação = " + bonificacao);
+this.salario = this.salario + bonificacao;
+}
+}*/
+
+
+
+
+
